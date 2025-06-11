abe020c4ead0ca77f1883e1352b347e206428932
nkieu-config <natthachak.config@gmail.com>
2025-06-11 20:50:51 +0700

    implement sort movements functionality

--- script.js
+++ script.js
@@ -62,7 +62,9 @@ const inputClosePin = document.querySelector(".form__input--pin");
 const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const html = `
@@ -224,4 +226,12 @@ btnClose.addEventListener("click", function (e) {
   }
 
   inputCloseUsername.value = inputClosePin.value = "";
+});
+
+// Handle sorting movements
+let sorted = false;
+btnSort.addEventListener("click", function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
